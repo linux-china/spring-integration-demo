1bd081ca192d6210a71b49bd3defaf6a9f392b47
linux_china <linux_china@hotmail.com>
2016-11-30 15:32:16 +0800

    endpoint components

---
+++ src/main/java/org/mvnsearch/integration/EndpointComponents.java
@@ -0,0 +1,11 @@
+package org.mvnsearch.integration;
+
+/**
+ * endpoint components
+ *
+ * @author linux_china
+ */
+public interface EndpointComponents {
+
+    public void placeOrder(Integer num);
+}
