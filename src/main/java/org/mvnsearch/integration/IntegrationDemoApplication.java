62e8db62ebd4f596175437f0257b4ddf4d75a79a
linux_china <linux_china@hotmail.com>
2016-11-30 15:33:15 +0800

    integration demo applicatoin

---
+++ src/main/java/org/mvnsearch/integration/IntegrationDemoApplication.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.http.config.EnableIntegrationGraphController;
+
+@SpringBootApplication
+@EnableIntegration
+@EnableIntegrationGraphController(path = "/testIntegration")
+public class IntegrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationDemoApplication.class, args);
+    }
+}
