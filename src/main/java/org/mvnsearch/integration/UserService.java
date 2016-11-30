793a3d551ac44c86eb1e5c471d27d517ad9c011b
linux_china <linux_china@hotmail.com>
2016-11-30 15:32:55 +0800

    user service

---
+++ src/main/java/org/mvnsearch/integration/UserService.java
@@ -0,0 +1,10 @@
+package org.mvnsearch.integration;
+
+/**
+ * user service
+ *
+ * @author linux_china
+ */
+public interface UserService {
+    public String defaultPayload(String fname, String lname);
+}
