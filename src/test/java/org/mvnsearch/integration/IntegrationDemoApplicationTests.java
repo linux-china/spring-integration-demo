12de6e0a92be5ed43c1359696e8ae6c596fc5e68
linux_china <linux_china@hotmail.com>
2016-11-30 15:34:22 +0800

    application test

---
+++ src/test/java/org/mvnsearch/integration/IntegrationDemoApplicationTests.java
@@ -0,0 +1,41 @@
+package org.mvnsearch.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.messaging.SubscribableChannel;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class IntegrationDemoApplicationTests {
+    @Autowired
+    @Qualifier("atomChannelMessageTemplate")
+    MessagingTemplate atomMessageTemplate;
+    @Autowired
+    @Qualifier("queueChannelMessageTemplate")
+    MessagingTemplate queueMessageTemplate;
+
+    @Autowired
+    @Qualifier("atomChannel")
+    SubscribableChannel messageChannel;
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    EndpointComponents gatwayService;
+
+    @Test
+    public void contextLoads() throws Exception {
+        userService.defaultPayload("Jacky", "chan");
+        gatwayService.placeOrder(333);
+        atomMessageTemplate.send(new GenericMessage<Integer>(111));
+        queueMessageTemplate.send(new GenericMessage<String>("2222"));
+        Thread.sleep(10000);
+    }
+
+}
