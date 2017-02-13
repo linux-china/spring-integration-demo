fcec6686d8c56c8b105c013a8cc74cbe5db1a497
linux_china <linux_china@hotmail.com>
2017-02-13 23:23:40 +0800

    update to spring boot 1.5

--- src/main/java/org/mvnsearch/integration/IntegrationDemoApplication.java
+++ src/main/java/org/mvnsearch/integration/IntegrationDemoApplication.java
@@ -2,11 +2,9 @@ package org.mvnsearch.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.http.config.EnableIntegrationGraphController;
 
 @SpringBootApplication
-@EnableIntegration
 @EnableIntegrationGraphController(path = "/testIntegration")
 public class IntegrationDemoApplication {
 
