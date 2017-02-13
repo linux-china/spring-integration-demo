28e00a3261c8f8b047714da9e4ff083243947766
linux_china <linux_china@hotmail.com>
2017-02-13 23:42:13 +0800

    add @InableIntegration

--- src/main/java/org/mvnsearch/integration/IntegrationDemoApplication.java
+++ src/main/java/org/mvnsearch/integration/IntegrationDemoApplication.java
@@ -2,10 +2,12 @@ package org.mvnsearch.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.http.config.EnableIntegrationGraphController;
 
 @SpringBootApplication
 @EnableIntegrationGraphController(path = "/testIntegration")
+@EnableIntegration
 public class IntegrationDemoApplication {
 
     public static void main(String[] args) {
