d4fb1fdb94ac54d9cac7825c6103f0940d97caaf
linux_china <libing.chen@gmail.com>
2021-07-16 13:43:53 -0700

    Migrated to junit 5

--- src/test/java/org/mvnsearch/integration/IntegrationDemoApplicationTests.java
+++ src/test/java/org/mvnsearch/integration/IntegrationDemoApplicationTests.java
@@ -1,16 +1,13 @@
 package org.mvnsearch.integration;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.messaging.SubscribableChannel;
 import org.springframework.messaging.support.GenericMessage;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class IntegrationDemoApplicationTests {
     @Autowired
